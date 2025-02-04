0d2e6876a00bd9a5315fbec40922e5ed15f26e0d
Kacperux1 <kacper34505@o2.pl>
2025-02-04 01:29:06 +0100

    new project structure

---
+++ src/main/java/pl/number11shop/Number11ShopApplication.java
@@ -0,0 +1,13 @@
+package pl.number11shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Number11ShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Number11ShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/number11shop/Number11ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.number11shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Number11ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
